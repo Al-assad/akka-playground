e92301e065bcfc695cb97291baf12e4b3ee6f9c8
Al-assad <yulin.ying@outlook.com>
2022-02-11 14:10:42 +0800

    update

---
+++ src/main/scala/com/github/al/assad/akka/cluster/ddata/package.scala
@@ -0,0 +1,15 @@
+package com.github.al.assad.akka.cluster
+
+import com.typesafe.config.ConfigFactory
+
+package object ddata {
+
+  val singleClusterConf = ConfigFactory.parseString(
+    """
+       akka.actor.provider = cluster
+       akka.remote.classic.netty.tcp.port = 0
+       akka.remote.artery.canonical.port = 0
+       akka.remote.artery.canonical.hostname = 127.0.0.1
+    """)
+
+}
