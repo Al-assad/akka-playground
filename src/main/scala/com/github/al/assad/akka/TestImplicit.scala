6602d16dcb2cafdae4ec94c46227d44d0d60dc76
Al-assad <yulin.ying@outlook.com>
2022-02-15 14:22:24 +0800

    update

--- src/main/scala/com/github/al/assad/akka/TestImplicit.scala
+++ src/main/scala/com/github/al/assad/akka/TestImplicit.scala
@@ -1,21 +1,43 @@
 package com.github.al.assad.akka
 
-import akka.actor.testkit.typed.scaladsl.TestProbe
-import akka.actor.typed.ActorRef
+import akka.actor.testkit.typed.scaladsl.{LogCapturing, TestProbe}
+import akka.actor.typed.{ActorRef, ActorSystem}
+import org.scalatest.wordspec.AnyWordSpecLike
 
 object TestImplicit {
 
   implicit class TestProbeWrapper[M](probe: TestProbe[M]) {
-
     def to[R](func: TestProbe[M] => R): R = func(probe)
-
     def from(func: ActorRef[M] => Unit): TestProbe[M] = {
       func(probe.ref)
       probe
     }
   }
+}
+
+
+trait STAkkaSpec extends AnyWordSpecLike with LogCapturing {
+
+  /*  implicit class TestProbeWrapper[M](probe: TestProbe[M]) {
+      def to[R](func: TestProbe[M] => R): R = func(probe)
+      def from(func: ActorRef[M] => Unit): TestProbe[M] = {
+        func(probe.ref)
+        probe
+      }
+    }*/
+
+  def probe[M](func: TestProbe[M] => Any)(implicit system: ActorSystem[Nothing]): TestProbe[M] = {
+    val probe = TestProbe[M]()
+    func(probe)
+    probe
+  }
+
+  def probeRef[M](func: ActorRef[M] => Any)(implicit system: ActorSystem[Nothing]): TestProbe[M] = {
+    val probe = TestProbe[M]()
+    func(probe.ref)
+    probe
+  }
 
 
 }
-
 
