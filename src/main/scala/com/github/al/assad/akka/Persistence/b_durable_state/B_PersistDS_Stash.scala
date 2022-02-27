0be944b111420636a645a697a61d5370bbe6c0f6
Al-assad <yulin.ying@outlook.com>
2022-02-27 15:37:40 +0800

    update

--- src/main/scala/com/github/al/assad/akka/Persistence/b_durable_state/B_PersistDS_Stash.scala
+++ src/main/scala/com/github/al/assad/akka/Persistence/b_durable_state/B_PersistDS_Stash.scala
@@ -1,47 +1,37 @@
-package com.github.al.assad.akka.Persistence.a_event_sourcing
+package com.github.al.assad.akka.Persistence.b_durable_state
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+
 import akka.actor.typed.{Behavior, SupervisorStrategy}
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
-import com.github.al.assad.akka.Persistence.{CborSerializable, inmenBackendConf}
+import akka.persistence.typed.state.scaladsl.{DurableStateBehavior, Effect}
+import com.github.al.assad.akka.Persistence.{AkkaPersistenceJdbcTestKit, CborSerializable, jdbcBackendJsonSerConf}
 import com.github.al.assad.akka.sleep
-import org.scalatest.wordspec.AnyWordSpecLike
 
 import scala.concurrent.duration.DurationInt
 
 /**
- * Akka Persistence Stash Example (Event Sourcing).
- *
- * https://doc.akka.io/docs/akka/current/typed/persistence.html#stash
+ * Akka Persistence Stash Example (Durable State).
+ * This is the Durable State version of [[com.github.al.assad.akka.Persistence.a_event_sourcing.PersistenceStash.SingleTaskScheduler]]
  */
-object PersistenceStash {
+object PersistDSStash {
 
-  /**
-   * The SingleTaskScheduler always allow only one task to be running.
-   */
+  //noinspection DuplicatedCode
   object SingleTaskScheduler {
+
     sealed trait Command extends CborSerializable
     final case class StartTask(taskId: String) extends Command
     final case class NextStep(taskId: String, instruction: String) extends Command
     final case class EndTask(taskId: String) extends Command
 
-    sealed trait Event extends CborSerializable
-    final case class TaskStarted(taskId: String) extends Event
-    final case class TaskStep(taskId: String, instruction: String) extends Event
-    final case class TaskCompleted(taskId: String) extends Event
-
     final case class State(taskIdInProcess: Option[String]) extends CborSerializable
 
-
-    //noinspection DuplicatedCode
     // command handler
-    private def onCommand(state: State, command: Command): Effect[Event, State] = {
+    private def onCommand(state: State, command: Command): Effect[State] = {
       state.taskIdInProcess match {
         case None => command match {
           // when no task is in process, only receive StartTask command
           case StartTask(taskId) =>
-            Effect.persist(TaskStarted(taskId)).thenRun(_ => s"taskId[$taskId] => started")
+            Effect.persist(State(Option(taskId))).thenRun(_ => s"taskId[$taskId] => started")
           case _ =>
             println(s"$command is drooped because no task is in process")
             Effect.unhandled
@@ -58,7 +48,7 @@ object PersistenceStash {
 
           case NextStep(taskId, instruction) =>
             if (inProgress == taskId) {
-              Effect.persist(TaskStep(taskId, instruction)).thenRun(_ => println(s"taskId[$taskId] => go to next step: $instruction"))
+              Effect.persist(state).thenRun(_ => println(s"taskId[$taskId] => go to next step: $instruction"))
             } else {
               println(s"$command is stashed because task[$inProgress] is in progress")
               Effect.stash // other task in progress, wait with new task until later
@@ -67,7 +57,7 @@ object PersistenceStash {
           case EndTask(taskId) =>
             if (inProgress == taskId) {
               println(s"taskId[$taskId] => completed")
-              Effect.persist(TaskCompleted(taskId)).thenUnstashAll()   // unstash all command
+              Effect.persist(State(None)).thenUnstashAll() // unstash all command
             } else {
               println(s"$command is stashed because task[$inProgress] is in progress")
               Effect.stash() //  other task in progress, wait with new task until later
@@ -76,35 +66,24 @@ object PersistenceStash {
       }
     }
 
-    // event handler
-    private def onEvent(state: State, event: Event): State = {
-      event match {
-        case TaskStarted(taskId) => State(Option(taskId))
-        case TaskStep(taskId, instruction) => state
-        case TaskCompleted(taskId) => State(None)
-      }
-    }
-
-    def apply(persistId: String): Behavior[Command] = EventSourcedBehavior[Command, Event, State](
-      persistenceId = PersistenceId.ofUniqueId(persistId),
+    def apply(id: String): Behavior[Command] = DurableStateBehavior[Command, State](
+      persistenceId = PersistenceId.ofUniqueId(id),
       emptyState = State(None),
-      commandHandler = (state, command) => onCommand(state, command),
-      eventHandler = (state, event) => onEvent(state, event)
-    ).onPersistFailure(SupervisorStrategy.restartWithBackoff(1.second, 10.seconds, 0.1))
-
+      commandHandler = onCommand)
+      .onPersistFailure(SupervisorStrategy.restartWithBackoff(1.second, 10.seconds, 0.1))
   }
 }
 
 
 //noinspection DuplicatedCode
-class PersistenceStashSpec extends ScalaTestWithActorTestKit(inmenBackendConf) with AnyWordSpecLike {
+class PersistDSStashSpec extends AkkaPersistenceJdbcTestKit(jdbcBackendJsonSerConf) {
 
-  import PersistenceStash._
-  import SingleTaskScheduler._
+  import PersistDSStash._
+  import PersistDSStash.SingleTaskScheduler._
 
   "SingleTaskScheduler" should {
     "behave normally" in {
-      val actor = spawn(SingleTaskScheduler("single-task-scheduler"))
+      val actor = spawn(SingleTaskScheduler("single-task-scheduler-ds"))
       actor ! StartTask("task-1")
       actor ! NextStep("task-1", "step-1")
       actor ! NextStep("task-1", "step-2")
@@ -112,14 +91,14 @@ class PersistenceStashSpec extends ScalaTestWithActorTestKit(inmenBackendConf) w
       sleep(3.seconds)
     }
     "behave when command been dropped" in {
-      val actor = spawn(SingleTaskScheduler("single-task-scheduler"))
+      val actor = spawn(SingleTaskScheduler("single-task-scheduler-ds"))
       actor ! NextStep("task-1", "step-1")
       actor ! NextStep("task-2", "step-1")
       actor ! EndTask("task-2")
       sleep(3.seconds)
     }
     "behave when command been stashed" in {
-      val actor = spawn(SingleTaskScheduler("single-task-scheduler"))
+      val actor = spawn(SingleTaskScheduler("single-task-scheduler-ds"))
       actor ! StartTask("task-1")
       actor ! StartTask("task-2")
       actor ! StartTask("task-3")
@@ -136,5 +115,5 @@ class PersistenceStashSpec extends ScalaTestWithActorTestKit(inmenBackendConf) w
     }
 
   }
-
 }
+
