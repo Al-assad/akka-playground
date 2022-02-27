f385610a8e3b020102783f0c640c2014af80513f
Al-assad <yulin.ying@outlook.com>
2022-02-27 15:57:51 +0800

    update

--- src/main/scala/com/github/al/assad/akka/Persistence/b_durable_state/C_PersistDS_Sample.scala
+++ src/main/scala/com/github/al/assad/akka/Persistence/b_durable_state/C_PersistDS_Sample.scala
@@ -1,27 +1,25 @@
-package com.github.al.assad.akka.Persistence.a_event_sourcing
+package com.github.al.assad.akka.Persistence.b_durable_state
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy}
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior, RetentionCriteria}
-import com.github.al.assad.akka.Persistence.{CborSerializable, inmenBackendConf}
+import akka.persistence.typed.state.scaladsl.{DurableStateBehavior, Effect}
+import com.github.al.assad.akka.Persistence.{AkkaPersistenceJdbcTestKit, CborSerializable, jdbcBackendJsonSerConf}
 import com.github.al.assad.akka.TestImplicit.TestProbeWrapper
-import org.scalatest.wordspec.AnyWordSpecLike
 
 import scala.concurrent.duration.DurationInt
 
-
 /**
  * A Complete Akka Persistence Example.
+ * This is the Durable State version of [[com.github.al.assad.akka.Persistence.a_event_sourcing.PersistenceSample]]
  */
 //noinspection DuplicatedCode
-object PersistenceSample {
+object PersistDSSample {
 
   object Articles {
 
     // cqrs command
     sealed trait Command extends CborSerializable
-    final case class Add(id: Int, title: String, replyTo: ActorRef[AddResult]) extends Command // or use ActorRef[StatusReply]
+    final case class Add(id: Int, title: String, replyTo: ActorRef[AddResult]) extends Command// or use ActorRef[StatusReply]
     final case class Publish(id: Int) extends Command
     final case class Remove(id: Int) extends Command
     final case class Like(id: Int) extends Command
@@ -30,14 +28,6 @@ object PersistenceSample {
     final case class GetSummary(replyTo: ActorRef[ArticleSummary]) extends Command
 
     final case class AddResult(success: Boolean, reason: Option[String] = None) extends CborSerializable
-
-    // cqrs event
-    sealed trait Event extends CborSerializable
-    final case class Added(id: Int, title: String) extends Event
-    final case class Published(id: Int) extends Event
-    final case class Removed(id: Int) extends Event
-    final case class Liked(id: Int) extends Event
-    final case object Cleared extends Event
 
     // state
     final case class State(items: Map[Int, Article]) extends CborSerializable {
@@ -59,46 +49,33 @@ object PersistenceSample {
     final case class ArticleSummary(articles: Seq[Article]) extends CborSerializable
 
     // command handler
-    private def onCommand(state: State, command: Command): Effect[Event, State] = command match {
+    private def onCommand(state: State, command: Command): Effect[State] = command match {
       case Add(id, title, replyTo) =>
         if (state.hasItem(id))
-          Effect.none.thenReply(replyTo)(state =>
-            AddResult(success = false, Some(s"Article already exists, title=${state.getItem(id).get.title}")))
+          Effect.reply(replyTo)(AddResult(success = false, Some(s"Article already exists, title=${state.getItem(id).get.title}")))
         else
-          Effect.persist(Added(id, title)).thenReply(replyTo)(_ => AddResult(success = true))
+          Effect.persist(state.addItem(id, title)).thenReply(replyTo)(_ => AddResult(success = true))
 
-      case Publish(id) => if (state.hasItem(id)) Effect.persist(Published(id)) else Effect.none
-      case Remove(id) => if (state.hasItem(id)) Effect.persist(Removed(id)) else Effect.none
-      case Like(id) => if (state.isPublish(id)) Effect.persist(Liked(id)) else Effect.none
-      case Clear => Effect.persist(Cleared)
+      case Publish(id) => if (state.hasItem(id)) Effect.persist(state.publishItem(id)) else Effect.none
+      case Remove(id) => if (state.hasItem(id)) Effect.persist(state.removeItem(id)) else Effect.none
+      case Like(id) => if (state.isPublish(id)) Effect.persist(state.likeItem(id)) else Effect.none
+      case Clear => Effect.persist(state.clearItems)
       case Get(id, replyTo) => Effect.reply(replyTo)(state.getItem(id))
       case GetSummary(replyTo) => Effect.reply(replyTo)(state.toSummary)
     }
 
-    // event handler
-    private def onEvent(state: State, event: Event): State = event match {
-      case Added(id, title) => state.addItem(id, title)
-      case Published(id) => state.publishItem(id)
-      case Removed(id) => state.removeItem(id)
-      case Liked(id) => state.likeItem(id)
-      case Cleared => state.clearItems
-    }
-
-
-    def apply(userId: String): Behavior[Command] = EventSourcedBehavior[Command, Event, State](
+    def apply(userId: String): Behavior[Command] = DurableStateBehavior[Command, State](
       persistenceId = PersistenceId.ofUniqueId(userId),
       emptyState = State.default,
-      commandHandler = (state, command) => onCommand(state, command),
-      eventHandler = (state, event) => onEvent(state, event))
-      .withRetention(RetentionCriteria.snapshotEvery(numberOfEvents = 100, keepNSnapshots = 2).withDeleteEventsOnSnapshot)
+      commandHandler = onCommand)
       .onPersistFailure(SupervisorStrategy.restartWithBackoff(200.millis, 5.seconds, 0.1))
   }
+
 }
 
 //noinspection DuplicatedCode
-class PersistenceSampleSpec extends ScalaTestWithActorTestKit(inmenBackendConf) with AnyWordSpecLike {
-
-  import PersistenceSample._
+class PersistDSSampleSpec extends AkkaPersistenceJdbcTestKit(jdbcBackendJsonSerConf, clearSchemaBeforeAll = true) {
+  import PersistDSSample._
   import Articles._
 
   "Articles" should {
@@ -134,5 +111,6 @@ class PersistenceSampleSpec extends ScalaTestWithActorTestKit(inmenBackendConf) 
       }
     }
   }
+
 
 }
